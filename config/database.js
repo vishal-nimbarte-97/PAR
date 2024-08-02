f391036220d38cfcd72e703f392c5d4949ed52a5
vishal-nimbarte-97 <vishalnimbarte097@gmail.com>
2024-08-02 10:37:57 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,26 @@
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
+
+// Initialize Sequelize with your database configuration
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+  }
+);
+
+// Function to authenticate the connection
+const connection = async () => {
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
+};
+
+// Export the Sequelize instance and the connection function
+module.exports = { sequelize, connection };
